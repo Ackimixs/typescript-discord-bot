88aa23faf3c5bddc2b19c5b4daaf583713a0cd62
Ackimixs <Ackimixs@protonmail.com>
2022-04-30 21:14:20 +0200

    auto remove task that have passed

--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -2,6 +2,8 @@ import { Client } from "discord.js";
 import { REST } from "@discordjs/rest"
 import { Routes } from "discord-api-types/v9";
 import { CommandList } from "../commands/_CommandList";
+import TaskModel from "../database/models/TaskModel";
+const CronJob = require('cron').CronJob;
 
 export const onReady = async (client: Client) => {
     const rest = new REST({ version: "9" }).setToken(
@@ -17,6 +19,26 @@ export const onReady = async (client: Client) => {
         ),
         { body: commandData }
     );
+
+
+    const checkTask = new CronJob(
+        '30 23 * * *',
+        async () => {
+            const TaskData = await TaskModel.find()
+            TaskData.forEach((task) => {
+                if (new Date(task.dueDate) < new Date()) {
+                    task.remove()
+                    console.log(`remove task : ${task.name}\n due date : ${task.dueDate}`)
+                }
+            })
+        }
+    )
+    checkTask.start()
+
+
+
+
+
 
     console.log(`[DiscordBot] - Connected on discord as ${client.user?.username}`)
 };
